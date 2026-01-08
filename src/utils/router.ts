6a44de9a53aba1411578e36a72dbe64a58abdc5d
ilona <ilona.leblanc@etudiant.univ-rennes.fr>
2026-01-08 15:46:04 +0100

    fix : router avec toutes les routes

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -2,8 +2,6 @@ import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue';
 import ClubForm from '../components/ClubForm.vue';
 import AdminDashboard from '../components/AdminDashboard.vue';
-
-
 import Profile from '../components/profile/Profile.vue';
 import Coach from '../components/coach/CoachDashboard.vue';
 import President from '../components/president/PresidentDashboard.vue';
@@ -15,7 +13,34 @@ const routes: Array<RouteRecordRaw> = [
     name: 'dashboard',
     component: HelloWorld,
   },
-  
+  {
+    path: '/admin/dashboard',
+    name: 'AdminDashboard',
+    component: AdminDashboard,
+  },
+  {
+    path: '/admin/newclub',
+    name: 'NewClub',
+    component: ClubForm,
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile,
+    props: route => ({
+      user: route.query.user
+    }),
+  },
+  {
+    path: '/coach',
+    name: 'Coach',
+    component: Coach,
+  },
+  {
+    path: '/president',
+    name: 'President',
+    component: President,
+  },
 ];
 
 const router = createRouter({
