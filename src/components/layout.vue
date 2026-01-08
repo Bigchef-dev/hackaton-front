58899e73bb4249bb1bac94a234ec10333fb39c1b
mbabinot <malo.babinot@etudiant.univ-rennes.fr>
2026-01-08 14:22:52 +0100

    suppr fichiers temp

--- src/components/layout.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-    import NavigationBar from './NavigationBar.vue';
-    import TestConnection from './TestConnection.vue';
-</script>
-<template>
-  <NavigationBar />
-  <TestConnection />
-</template>
-
